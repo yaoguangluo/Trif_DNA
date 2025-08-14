a51c59fc4da28b766b3bf7d65bb988f2429dfaa7
浏阳德塔软件开发有限公司, 430181198505250014 <yaoguangluo@outlook.com>
2025-08-14 13:06:04 +0800

    unit test shell
    # --Refer--
    ##### --个人作者:罗瑶光--
    ###### /*
    ###### * 个人著作权人, 第一作者: 罗瑶光, 1985年5月25日出生，性别男, 湖南省浏阳市人.
    ###### * yaoguangluo@outlook.com, 313699483@qq.com, 2080315360@qq.com,
    ###### *（lyg.tin@gmail.com2018年后因G网屏蔽不再使用）
    ###### * 15116110525-（唯一标识）
    ###### * 430181198505250014, G24402609, EB0581342
    ###### * 204925063, 389418686, F2406501, 0626136
    ###### * 当前居住地址: 湖南省 浏阳市 集里街道 神仙坳社区 大塘冲路一段 208号 阳光家园别墅小区 第十栋别墅
    ###### * */

---
+++ SRC/test/java/ME/VPC/S/buttons/Page_X_S_sButtonsreturnEnglishTest.java
@@ -0,0 +1,25 @@
+package test.java.ME.VPC.S.buttons;
+
+import org.junit.jupiter.api.Test;
+/*
+ * 著作权人, 作者 罗瑶光, 浏阳
+ * yaoguangluo@outlook.com, 313699483@qq.com, 2080315360@qq.com,
+* (lyg.tin@gmail.com2018年后因G网屏蔽不再使用）
+ 
+ ** 15116110525-
+ * 430181198505250014, G24402609, EB0581342
+ * 204925063, 389418686, F2406501, 0626136
+ * 湖南省 浏阳市 集里街道 神仙坳社区 大塘冲路一段
+*  208号 阳光家园别墅小区 第十栋别墅
+ * */
+class Page_X_S_sButtonsreturnEnglishTest {
+
+    @Test
+    void testData_X_returnEnglish() {
+        // mock trif
+//        DetaButton result = Page_X_S_sButtonsreturnEnglish.data_X_returnEnglish(new Page_X_S_sVSQ(), new App());
+//        Assertions.assertEquals(new DetaButton("string", 0, 0, null), result);
+    }
+}
+
+//Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
