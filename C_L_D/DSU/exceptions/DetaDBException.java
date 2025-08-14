7db8bc8960131000cb8c6dc503419198eac79f7f
浏阳德塔软件开发有限公司, 430181198505250014 <yaoguangluo@outlook.com>
2025-08-14 09:33:32 +0800

    CLE上传
    /*
     * 著作权人, 作者 罗瑶光, 浏阳
     * yaoguangluo@outlook.com, 313699483@qq.com, 2080315360@qq.com,
     * (lyg.tin@gmail.com2018年回国后因国内G网屏蔽不再使用）
     * 15116110525-
     * 430181198505250014, G24402609, EB0581342
     * 204925063, 389418686, F2406501, 0626136
     * 湖南省 浏阳市 集里街道 神仙坳社区 大塘冲一段路 208号 阳光家园别墅小区 第十栋别墅
     * */

---
+++ C_L_D/DSU/exceptions/DetaDBException.java
@@ -0,0 +1,42 @@
+package DSU.exceptions;
+
+/*
+ * 著作权人, 作者 罗瑶光, 浏阳
+ * yaoguangluo@outlook.com, 313699483@qq.com, 2080315360@qq.com, 
+ * (lyg.tin@gmail.com2018年回国后因国内G网屏蔽不再使用）
+ * 15116110525-
+ * 430181198505250014, G24402609, EB0581342
+ * 204925063, 389418686, F2406501, 0626136
+ * 湖南省 浏阳市 集里街道 神仙坳社区 大塘冲一段路 208号 阳光家园别墅小区 第十栋别墅
+ * */
+public class DetaDBException extends RuntimeException {
+	
+	private String errorId;
+	private String errorMessage;
+
+	public String getErrorId() {
+		return errorId;
+	}
+
+	public void I_ErrorId(String errorId) {
+		this.errorId= errorId;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void I_ErrorMessage(String errorMessage) {
+		this.errorMessage= errorMessage;
+	}
+
+	public DetaDBException() {
+		super();
+	}
+
+	public DetaDBException(String errorId, String errorMessage) {
+		super(errorMessage);
+		this.errorId= errorId;
+		this.errorMessage= errorMessage;
+	}
+}
---
+++ C_L_D/DSU/exceptions/DetaDBExceptionTest.java
@@ -0,0 +1,24 @@
+package DSU.exceptions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DetaDBExceptionTest {
+
+    @Test
+    void getErrorId() {
+    }
+
+    @Test
+    void i_ErrorId() {
+    }
+
+    @Test
+    void getErrorMessage() {
+    }
+
+    @Test
+    void i_ErrorMessage() {
+    }
+}
