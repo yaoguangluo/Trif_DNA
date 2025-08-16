f0d336de3edc6637613dc55c52b76ef7c6e0e1cf
浏阳德塔软件开发有限公司, 430181198505250014 <yaoguangluo@outlook.com>
2025-08-16 11:51:09 +0800

    Update StaticReflectionMapQ_VECS_C.java
    /*
     * 著作权人, 作者 罗瑶光, 浏阳
     * yaoguangluo@outlook.com, 313699483@qq.com, 2080315360@qq.com,
     * (lyg.tin@gmail.com2018年回国后因国内G网屏蔽不再使用）
     * 15116110525-
     * 430181198505250014, G24402609, EB0581342
     * 204925063, 389418686, F2406501, 0626136
     * 湖南省 浏阳市 集里街道 神仙坳社区 大塘冲一段路 208号 阳光家园别墅小区 第十栋别墅
     * */

--- C_L_S/S_A/SEM/reflection/StaticReflectionMapQ_VECS_C.java
+++ C_L_S/S_A/SEM/reflection/StaticReflectionMapQ_VECS_C.java
@@ -14,32 +14,35 @@ import S_A.pheromone.IMV_SIQ;
  * 430181198505250014, G24402609, EB0581342
  * 204925063, 389418686, F2406501, 0626136
  * 湖南省 浏阳市 集里街道 神仙坳社区 大塘冲路一段
-*  208号 阳光家园别墅小区 第十栋
+ *  208号 阳光家园别墅小区 第十栋
  * */
 public class StaticReflectionMapQ_VECS_C {
-	public static IMV_SIQ annotationMap= new IMV_SIQ();
-	@SuppressWarnings("unchecked")
-	public static void callFunction(CallFunctionKey callFunctionKey
-			, StaticFunctionMapQ_VECS_E staticFunctionMapQ_VECS_C
-			, IMV_SIQ output, App NE)  {
-		String[] 传参因子= (String[]) output.get("传参因子");
-		int 因子= 0;
-		Object map=null;
-		if(callFunctionKey.contains("searchFromTable", NE)){
-			IMV_SIQ inputValues= StaticFunctionMap.preValues(output, 传参因子);
-			if(inputValues.getBoolean("find")) {
-				staticFunctionMapQ_VECS_C.searchFromTable(NE);
-			}
-			StaticFunctionMap.postValues(output, inputValues.getBoolean("find"), map
-					, "searchFromTable");
-		};
-		if(callFunctionKey.contains("searchFromTablewithScale", NE)){
-			IMV_SIQ inputValues= StaticFunctionMap.preValues(output, 传参因子);
-			if(inputValues.getBoolean("find")) {
-				map= staticFunctionMapQ_VECS_C.searchFromTablewithScale(NE);
-			}
-			StaticFunctionMap.postValues(output, inputValues.getBoolean("find"), map
-					, "searchFromTablewithScale");
-		};
-	}
+    public static IMV_SIQ annotationMap = new IMV_SIQ();
+
+    @SuppressWarnings("unchecked")
+    public static void callFunction(CallFunctionKey callFunctionKey
+        , StaticFunctionMapQ_VECS_E staticFunctionMapQ_VECS_C
+        , IMV_SIQ output, App NE) {
+        String[] 传参因子 = (String[]) output.get("传参因子");
+        int 因子 = 0;
+        Object map = null;
+        if (callFunctionKey.contains("searchFromTable", NE)) {
+            IMV_SIQ inputValues = StaticFunctionMap.preValues(output, 传参因子);
+            if (inputValues.getBoolean("find")) {
+                staticFunctionMapQ_VECS_C.searchFromTable(NE, 0);
+            }
+            StaticFunctionMap.postValues(output, inputValues.getBoolean("find"), map
+                , "searchFromTable");
+        }
+        ;
+        if (callFunctionKey.contains("searchFromTablewithScale", NE)) {
+            IMV_SIQ inputValues = StaticFunctionMap.preValues(output, 传参因子);
+            if (inputValues.getBoolean("find")) {
+                map = staticFunctionMapQ_VECS_C.searchFromTablewithScale(NE, 0, false);
+            }
+            StaticFunctionMap.postValues(output, inputValues.getBoolean("find"), map
+                , "searchFromTablewithScale");
+        }
+        ;
+    }
 }
