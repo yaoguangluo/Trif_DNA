141586860835a68549a34083c7db79cebf43bb19
浏阳德塔软件开发有限公司, 430181198505250014 <yaoguangluo@outlook.com>
2025-08-16 11:48:04 +0800

    Update StaticFunctionMapQ_VECS_C.java
    /*
     * 著作权人, 作者 罗瑶光, 浏阳
     * yaoguangluo@outlook.com, 313699483@qq.com, 2080315360@qq.com,
     * (lyg.tin@gmail.com2018年回国后因国内G网屏蔽不再使用）
     * 15116110525-
     * 430181198505250014, G24402609, EB0581342
     * 204925063, 389418686, F2406501, 0626136
     * 湖南省 浏阳市 集里街道 神仙坳社区 大塘冲一段路 208号 阳光家园别墅小区 第十栋别墅
     * */

--- C_L_S/S_A/SEM/bloom/StaticFunctionMapQ_VECS_C.java
+++ C_L_S/S_A/SEM/bloom/StaticFunctionMapQ_VECS_C.java
@@ -5,13 +5,22 @@ import S_A.pheromone.IMV_SIQ;
 
 import java.util.List;
 
-//著作权人+ 作者= 罗瑶光
+/*
+ * 著作权人, 作者 罗瑶光, 浏阳
+ * yaoguangluo@outlook.com, 313699483@qq.com, 2080315360@qq.com,
+ * (lyg.tin@gmail.com2018年回国后因国内G网屏蔽不再使用）
+ * 15116110525-
+ * 430181198505250014, G24402609, EB0581342
+ * 204925063, 389418686, F2406501, 0626136
+ * 湖南省 浏阳市 集里街道 神仙坳社区 大塘冲一段路 208号 阳光家园别墅小区 第十栋别墅
+ * */
+//著作权人 + 作者 = 罗瑶光
 //Refer的源码来自 《DNA 元基催化与肽计算 第三修订版本 V039010912》
 //证书编号：国作登字-2021-L-00268255 (中华人民共和国 国家版权登记中心)
 public interface StaticFunctionMapQ_VECS_C {
     public static IMV_SIQ annotationMap = new IMV_SIQ();
 
-    public void searchFromTable(App NE);
+    public void searchFromTable(App NE, int rangeScale);
 
-    public List<Object[]> searchFromTablewithScale(App NE);
+    public List<Object[]> searchFromTablewithScale(App NE, int rangeScale, boolean isKey);
 }	
