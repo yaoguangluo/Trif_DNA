ba7b809ecaa53da5c70406a98551868d1d87092a
浏阳德塔软件开发有限公司, 430181198505250014 <yaoguangluo@outlook.com>
2025-08-19 11:16:21 +0800

    Update LYG9DWithDoubleTopSort4DTest.java
    /*
     * 著作权人, 作者 罗瑶光, 浏阳
     * yaoguangluo@outlook.com, 313699483@qq.com, 2080315360@qq.com,
     * (lyg.tin@gmail.com2018年回国后因国内G网屏蔽不再使用）
     * 15116110525-
     * 430181198505250014, G24402609, EB0581342
     * 204925063, 389418686, F2406501, 0626136
     * 湖南省 浏阳市 集里街道 神仙坳社区 大塘冲一段路 208号 阳光家园别墅小区 第十栋别墅
     * */

--- SRC/test/java/InterfaceTest/sort/arraySort/LYG4DQS4D/LYG9DWithDoubleTopSort4DTest.java
+++ SRC/test/java/InterfaceTest/sort/arraySort/LYG4DQS4D/LYG9DWithDoubleTopSort4DTest.java
@@ -3,6 +3,7 @@ package test.java.InterfaceTest.sort.arraySort.LYG4DQS4D;
 import A_V.ASQ.PSU.test.TimeCheck;
 import U_V.OEU.LYG4DQS4D.LYG9DWithDoubleTopSort4D;
 import org.junit.jupiter.api.Test;
+
 /*
  * 著作权人, 作者 罗瑶光, 浏阳
  * yaoguangluo@outlook.com, 313699483@qq.com, 2080315360@qq.com,
@@ -14,11 +15,11 @@ import org.junit.jupiter.api.Test;
  * */
 //重要的本人常用排序接口的测试main移到test单元文件中。
 /*
- * 这是一个 用于德塔快速排序4代进行左右比对分词优化的算法对double数组排序demo 测试main函数demo的test版本，
- * 在导入了api之后进行系统集成，然后
- * 用下面的对应的函数中源码逻辑进行复制粘贴到工程中，直接运行，即可出结果，源码的逻辑按照输入准备计算的参数，
- * 然后执行，然后获取输出需要的结果，可以用断点来查看数据，也可以用println来显示输出，方便集成，对程序员友好。
- * 系统需要jdk1.8 以上的java环境，本人会把测试的输入输出都注释在这个文件里。及其傻瓜化的流程，方便商业化落地。
+ * 这是一个 用于德塔快速排序4代进行左右比对分词优化的算法对double数组排序demo 测试main函数demo的
+ * test版本，在导入了api之后进行系统集成，然后用下面的对应的函数中源码逻辑进行复制粘贴到工程中，直接
+ * 运行，即可出结果，源码的逻辑按照输入准备计算的参数，然后执行，然后获取输出需要的结果，可以用断点来
+ * 查看数据，也可以用println来显示输出，方便集成，对程序员友好。系统需要jdk1.8 以上的java环境，
+ * 本人会把测试的输入输出都注释在这个文件里。及其傻瓜化的流程，方便商业化落地。
  * --罗瑶光
  * */
 class LYG9DWithDoubleTopSort4DTest {
@@ -33,7 +34,8 @@ class LYG9DWithDoubleTopSort4DTest {
         for (int i = 0; i < doubles.length; i++) {
             doubles[i] = Math.random();
         }
-        LYG9DWithDoubleTopSort4D lYG9DWithDoubleTopSort2D = new LYG9DWithDoubleTopSort4D();
+        LYG9DWithDoubleTopSort4D lYG9DWithDoubleTopSort2D
+            = new LYG9DWithDoubleTopSort4D();
         TimeCheck timecheck = new TimeCheck();
         timecheck.begin();
         lYG9DWithDoubleTopSort2D.sort(doubles, 7, 70);
