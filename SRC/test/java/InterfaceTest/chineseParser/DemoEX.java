3169698d056ace5bf215ddfbc6ada336bc52fa08
浏阳德塔软件开发有限公司, 430181198505250014 <yaoguangluo@outlook.com>
2025-08-19 10:28:15 +0800

    Update DemoEX.java
    /*
     * 著作权人, 作者 罗瑶光, 浏阳
     * yaoguangluo@outlook.com, 313699483@qq.com, 2080315360@qq.com,
     * (lyg.tin@gmail.com2018年回国后因国内G网屏蔽不再使用）
     * 15116110525-
     * 430181198505250014, G24402609, EB0581342
     * 204925063, 389418686, F2406501, 0626136
     * 湖南省 浏阳市 集里街道 神仙坳社区 大塘冲一段路 208号 阳光家园别墅小区 第十栋别墅
     * */

--- SRC/test/java/InterfaceTest/chineseParser/DemoEX.java
+++ SRC/test/java/InterfaceTest/chineseParser/DemoEX.java
@@ -20,10 +20,10 @@ import java.util.List;
  * 湖南省 浏阳市 集里街道 神仙坳社区 大塘冲一段路 208号 阳光家园别墅小区 第十栋别墅
  * */
 /*
- * 这是一个 极速分词 测试main函数demo的test版本，在导入了api之后进行系统集成，然后用下面的对应的函数中源码逻辑
- * 进行复制粘贴到工程中，直接运行，即可出结果，源码的逻辑按照输入准备计算的参数，然后执行，然后获取输出需要的结果，
- * 可以用断点来查看数据，也可以用println来显示输出，方便集成，对程序员友好。系统需要jdk1.8 以上的java环境，
- * 本人会把测试的输入输出都注释在这个文件里。及其傻瓜化的流程，方便商业化落地。
+ * 这是一个 极速分词 测试main函数demo的test版本，在导入了api之后进行系统集成，然后用下面的对应的函数中
+ * 源码逻辑进行复制粘贴到工程中，直接运行，即可出结果，源码的逻辑按照输入准备计算的参数，然后执行，然后获取
+ * 输出需要的结果，可以用断点来查看数据，也可以用println来显示输出，方便集成，对程序员友好。系统需要jdk1.8
+ * 以上的java环境，本人会把测试的输入输出都注释在这个文件里。及其傻瓜化的流程，方便商业化落地。
  * --罗瑶光
  * */
 public class DemoEX {
@@ -71,7 +71,7 @@ public class DemoEX {
         System.out.println("-展示词频统计-");
         IMV_SIQ_S_ fwa = commonTestInition.NE.app_S._A.getWordFrequencyByReturnSortMap(sets
             , commonTestInition.NE);
-        //		t.end();
+        //t.end();
         for (int i = fwa.size() - 1; i >= 0; i--) {
             System.out.print(fwa.get(i).get_word() + S_Common.STRING_SYMBOL_PER
                 + fwa.get(i).get_frequency() + "----");
@@ -81,46 +81,46 @@ public class DemoEX {
     }
 }
 //64
-//			EmotionMap_E emotionMap_E=new EmotionMap_E();
-//			NERO_C_OneTime_E nERO_C_OneTime_E=
-//			Nlp_CE_X_S nlp_CE_X_S=
-//		  Pos_X_P pos_X_P=
-//			SensingTest sensingTest=
-//			FMHMMListOneTime_E fMHMMListOneTime_E=
-//			Quick6DLuoYaoguangSort3DMap_E quick6DLuoYaoguangSort3DMap_E=
+//EmotionMap_E emotionMap_E=new EmotionMap_E();
+//NERO_C_OneTime_E nERO_C_OneTime_E=
+//Nlp_CE_X_S nlp_CE_X_S=
+//  Pos_X_P pos_X_P=
+//SensingTest sensingTest=
+//FMHMMListOneTime_E fMHMMListOneTime_E=
+//Quick6DLuoYaoguangSort3DMap_E quick6DLuoYaoguangSort3DMap_E=
 
-//		NERO_C_OneTime_E nERO_C_OneTime_E = new NERO_C_OneTime_E();
-//		FMHMMListOneTime_E fMHMMListOneTime_E = new FMHMMListOneTime_E();
-//		this.fHMMList = new FMHMMListOneTime_E();
-//		_A.forestRoots = fHMMList.getMap();
-//		FMHMMListOneTime_E_X_S fMHMMListOneTime_E_X_S = (FMHMMListOneTime_E_X_S)fHMMList;
-//		wordsForest = fMHMMListOneTime_E_X_S.posCnToCn;
-//		pos_C = new Pos_X_P();
-//		nlp_C = new Nlp_CE_X_S();
-//		nlp_C.pos_X_P = pos_C;
-//		nero_C = new NERO_C_OneTime_E();
-//		emotionMap = new EmotionMap_E();
-//		sensingTest = new SensingTest();
-//		quick6DLuoYaoguangSort = new Quick6DLuoYaoguangSort3DMap_E();
+//NERO_C_OneTime_E nERO_C_OneTime_E = new NERO_C_OneTime_E();
+//FMHMMListOneTime_E fMHMMListOneTime_E = new FMHMMListOneTime_E();
+//this.fHMMList = new FMHMMListOneTime_E();
+//_A.forestRoots = fHMMList.getMap();
+//FMHMMListOneTime_E_X_S fMHMMListOneTime_E_X_S = (FMHMMListOneTime_E_X_S)fHMMList;
+//wordsForest = fMHMMListOneTime_E_X_S.posCnToCn;
+//pos_C = new Pos_X_P();
+//nlp_C = new Nlp_CE_X_S();
+//nlp_C.pos_X_P = pos_C;
+//nero_C = new NERO_C_OneTime_E();
+//emotionMap = new EmotionMap_E();
+//sensingTest = new SensingTest();
+//quick6DLuoYaoguangSort = new Quick6DLuoYaoguangSort3DMap_E();
 
 //186
 //20230106-System.out.println(S_Count.a1);
 //20230106-System.out.println(S_Count.a2);
 //20230106-System.out.println(S_Count.a3);
 //20230106-System.out.println(S_Count.a4);
-//		//20230106-System.out.println(S_Count.a5);
-//		//20230106-System.out.println(S_Count.a6);
-//		//20230106-System.out.println(S_Count.a7);
-//		//20230106-System.out.println(S_Count.a8);
-//		//20230106-System.out.println(S_Count.a9);
-//		//20230106-System.out.println(S_Count.a10);
-//		//20230106-System.out.println(S_Count.a11);
-//		//20230106-System.out.println(S_Count.a12);
+//20230106-System.out.println(S_Count.a5);
+//20230106-System.out.println(S_Count.a6);
+//20230106-System.out.println(S_Count.a7);
+//20230106-System.out.println(S_Count.a8);
+//20230106-System.out.println(S_Count.a9);
+//20230106-System.out.println(S_Count.a10);
+//20230106-System.out.println(S_Count.a11);
+//20230106-System.out.println(S_Count.a12);
 //68
-//        for (int j = 0; j < 1; j++) {
-//            for (int i = 0; i < sets.size(); i++) {
-//                if (!sets.get(i).replaceAll("\\s+", "").equals("")) {
-//                    //System.out.print(sets.get(i) + "/" + pos.get(sets.get(i)) + "----");
-//                }
-//            }
+//for (int j = 0; j < 1; j++) {
+//    for (int i = 0; i < sets.size(); i++) {
+//        if (!sets.get(i).replaceAll("\\s+", "").equals("")) {
+//            //System.out.print(sets.get(i) + "/" + pos.get(sets.get(i)) + "----");
 //        }
+//    }
+//}
