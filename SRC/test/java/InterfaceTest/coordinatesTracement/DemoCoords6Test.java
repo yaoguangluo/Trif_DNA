509089bbe539be12faca49974ac3d1ea33d843ff
浏阳德塔软件开发有限公司, 430181198505250014 <yaoguangluo@outlook.com>
2025-08-19 11:11:55 +0800

    Update DemoCoords6Test.java
    /*
     * 著作权人, 作者 罗瑶光, 浏阳
     * yaoguangluo@outlook.com, 313699483@qq.com, 2080315360@qq.com,
     * (lyg.tin@gmail.com2018年回国后因国内G网屏蔽不再使用）
     * 15116110525-
     * 430181198505250014, G24402609, EB0581342
     * 204925063, 389418686, F2406501, 0626136
     * 湖南省 浏阳市 集里街道 神仙坳社区 大塘冲一段路 208号 阳光家园别墅小区 第十栋别墅
     * */

--- SRC/test/java/InterfaceTest/coordinatesTracement/DemoCoords6Test.java
+++ SRC/test/java/InterfaceTest/coordinatesTracement/DemoCoords6Test.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+
 /*
  * 著作权人, 作者 罗瑶光, 浏阳
  * yaoguangluo@outlook.com, 313699483@qq.com, 2080315360@qq.com,
@@ -21,10 +22,11 @@ import java.util.Map;
  * 湖南省 浏阳市 集里街道 神仙坳社区 大塘冲一段路 208号 阳光家园别墅小区 第十栋别墅
  * */
 /*
- * 这是一个 处理fusion 先 fissile 采样 然后fusion 1 观测heart Euclid_X_findHeartPosition2D 测试main函数demo的test版本，在导入了api之后进行系统集成，然后
- * 用下面的对应的函数中源码逻辑进行复制粘贴到工程中，直接运行，即可出结果，源码的逻辑按照输入准备计算的参数，
- * 然后执行，然后获取输出需要的结果，可以用断点来查看数据，也可以用println来显示输出，方便集成，对程序员友好。
- * 系统需要jdk1.8 以上的java环境，本人会把测试的输入输出都注释在这个文件里。及其傻瓜化的流程，方便商业化落地。
+ * 这是一个 处理fusion 先 fissile 采样 然后fusion 1 观测heart Euclid_X_findHeartPosition2D
+ * 测试main函数demo的test版本，在导入了api之后进行系统集成，然后用下面的对应的函数中源码逻辑进行复制
+ * 粘贴到工程中，直接运行，即可出结果，源码的逻辑按照输入准备计算的参数，然后执行，然后获取输出需要的结果
+ * ，可以用断点来查看数据，也可以用println来显示输出，方便集成，对程序员友好。系统需要jdk1.8 以上的
+ * java环境，本人会把测试的输入输出都注释在这个文件里。及其傻瓜化的流程，方便商业化落地。
  * --罗瑶光
  * */
 @SuppressWarnings("unused")
@@ -60,7 +62,8 @@ public class DemoCoords6Test {
 
 //
         //处理fusion 先 fissile 采样
-        Map<Double, List<AMV_MVS_VSQ_2D>> output = Fissile_X_fissilePosition2D._E(input, 13);
+        Map<Double, List<AMV_MVS_VSQ_2D>> output
+            = Fissile_X_fissilePosition2D._E(input, 13);
         Iterator<Double> iterator = output.keySet().iterator();
         while (iterator.hasNext()) {
             double temp = iterator.next();
@@ -82,7 +85,8 @@ public class DemoCoords6Test {
             groupsHeart.put(temp, heartTemp);
         }
 //		//2观测结果 用10 和11分别带入可测试。
-//		Map<Double, List<AMV_MVS_VSQ_2D>> fusion= Fusion.fusionAMV_MVS_VSQ_2DwithHeart(output, groupsHeart, 11);
+//		Map<Double, List<AMV_MVS_VSQ_2D>> fusion
+//		= Fusion.fusionAMV_MVS_VSQ_2DwithHeart(output, groupsHeart, 11);
 //		Iterator<Double> iterator3= fusion.keySet().iterator();
 //		while(iterator3.hasNext()) {
 //			double temp= iterator3.next();
@@ -101,7 +105,9 @@ public class DemoCoords6Test {
 //	
 //		Map<Double, List<AMV_MVS_VSQ_2D>>inputMap= new HashMap<>();
 //		inputMap.put(0.0, input);
-//		Map<Double, List<AMV_MVS_VSQ_2D>>output= PositionClasification.addNewPositionWithoutHeart(inputMap, position2D5, 29);//采样距离28.4
+//		Map<Double, List<AMV_MVS_VSQ_2D>>output
+//		= PositionClasification.addNewPositionWithoutHeart(inputMap
+//		, position2D5, 29);//采样距离28.4
 //		
 //		Iterator<Double> iterator= output.keySet().iterator();
 //		while(iterator.hasNext()) {
@@ -122,7 +128,8 @@ public class DemoCoords6Test {
         input.add(position2D5);
         //double v= FindPCAMeanDistance.findMeanDistanceFromPositions2D(input, 5, 4);
         //System.out.println(v);
-        double[] v = Distance_X_findPascalMeanDistanceByEachPositions2D._E(input, 3, 4);
+        double[] v = Distance_X_findPascalMeanDistanceByEachPositions2D._E(input
+            , 3, 4);
         for (int i = 0; i < v.length; i++) {
             System.out.println(v[i]);
         }
@@ -130,7 +137,8 @@ public class DemoCoords6Test {
 
     public static void _E(List<AMV_MVS_VSQ_2D> inputs) {
         //处理fusion 先 fissile 采样
-        Map<Double, List<AMV_MVS_VSQ_2D>> output = Fissile_X_fissilePosition2D._E(inputs, 13);
+        Map<Double, List<AMV_MVS_VSQ_2D>> output
+            = Fissile_X_fissilePosition2D._E(inputs, 13);
         Iterator<Double> iterator = output.keySet().iterator();
         while (iterator.hasNext()) {
             double temp = iterator.next();
@@ -151,8 +159,19 @@ public class DemoCoords6Test {
             System.out.println("hearts->" + heartTemp.getX() + ":" + heartTemp.getY());
             groupsHeart.put(temp, heartTemp);
         }
+        //double v= FindPCAMeanDistance.findMeanDistanceFromPositions2D(input, 5, 4);
+        //System.out.println(v);
+        double[] v = Distance_X_findPascalMeanDistanceByEachPositions2D._E(inputs
+            , 3, 4);
+        for (int i = 0; i < v.length; i++) {
+            System.out.println(v[i]);
+        }
+    }
+}
+//160
 //		//2观测结果 用10 和11分别带入可测试。
-//		Map<Double, List<AMV_MVS_VSQ_2D>> fusion= Fusion.fusionAMV_MVS_VSQ_2DwithHeart(output, groupsHeart, 11);
+//		Map<Double, List<AMV_MVS_VSQ_2D>> fusion= Fusion.fusionAMV_MVS_VSQ_2DwithHeart(
+//		output, groupsHeart, 11);
 //		Iterator<Double> iterator3= fusion.keySet().iterator();
 //		while(iterator3.hasNext()) {
 //			double temp= iterator3.next();
@@ -164,14 +183,16 @@ public class DemoCoords6Test {
 //				System.out.println(temp1.getX()+ ":"+ temp1.getY());
 //			}
 //		}
-        //
+//
 //		AMV_MVS_VSQ_2D position2D5= new AMV_MVS_VSQ_2D();
 //		position2D5.I_X(15);
 //		position2D5.I_Y(25);
 //
 //		Map<Double, List<AMV_MVS_VSQ_2D>>inputMap= new HashMap<>();
 //		inputMap.put(0.0, input);
-//		Map<Double, List<AMV_MVS_VSQ_2D>>output= PositionClasification.addNewPositionWithoutHeart(inputMap, position2D5, 29);//采样距离28.4
+//		Map<Double, List<AMV_MVS_VSQ_2D>>output
+//		= PositionClasification.addNewPositionWithoutHeart(inputMap, position2D5, 29);
+// 采样距离28.4
 //
 //		Iterator<Double> iterator= output.keySet().iterator();
 //		while(iterator.hasNext()) {
@@ -185,16 +206,8 @@ public class DemoCoords6Test {
 //			}
 //		}
 
-        //
+//
 //        AMV_MVS_VSQ_2D position2D5 = new AMV_MVS_VSQ_2D();
 //        position2D5.I_X(15);
 //        position2D5.I_Y(25);
 //        inputs.add(position2D5);
-        //double v= FindPCAMeanDistance.findMeanDistanceFromPositions2D(input, 5, 4);
-        //System.out.println(v);
-        double[] v = Distance_X_findPascalMeanDistanceByEachPositions2D._E(inputs, 3, 4);
-        for (int i = 0; i < v.length; i++) {
-            System.out.println(v[i]);
-        }
-    }
-}
