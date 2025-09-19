445ac586dcaea07bfdfb47cec84cf3c47224080d
luoyaoguang430181198505250014 <yaoguangluo@outlook.com>
2025-09-20 00:32:39 +0800

    1 思考下这几天要做些什么。 2 优化细化测试函数。
    /*
     * 著作权人, 作者 始祖 罗瑶光, 浏阳
     * yaoguangluo@outlook.com, 313699483@qq.com, 2080315360@qq.com,
     * (lyg.tin@gmail.com2018年回国后因国内G网屏蔽不再使用）
     * 15116110525-
     * 430181198505250014, G24402609, EB0581342
     * 204925063, 389418686, F2406501, 0626136
     * 湖南省 浏阳市 集里街道 神仙坳社区 大塘冲一段路 208号 阳光家园别墅小区 第十栋别墅 第三层
     * */

---
+++ src/test/java/InterfaceTest/tinShell/FastCartesianIdentifyTest.java
@@ -0,0 +1,81 @@
+package test.java.InterfaceTest.tinShell;
+
+import java.util.HashMap;
+
+/*
+ * 著作权人, 作者 罗瑶光, 浏阳
+ * yaoguangluo@outlook.com, 313699483@qq.com, 2080315360@qq.com,
+ * (lyg.tin@gmail.com2018年回国后因国内G网屏蔽不再使用）
+ * 15116110525-
+ * 430181198505250014, G24402609, EB0581342
+ * 204925063, 389418686, F2406501, 0626136
+ * 湖南省 浏阳市 集里街道 神仙坳社区 大塘冲一段路 208号 阳光家园别墅小区 第十栋别墅
+ * */
+public class FastCartesianIdentifyTest {
+	/*
+	 * 思考2点
+	 * 
+	 * CN 1- 之前的意识错误纠正，主要是思维方式的纠正，之前我把2018年分词
+	 * 还是用老版 Lucene，把落后归咎于业界的不争气。在这一点上我还是要诚恳
+	 * 地道歉，做人应该符合科学的方法论 和 合理的制度规范 去 思考论题。不能
+	 * 激进。好比我写函数思想一样，也不能图快和激进，因为我的目标一直很明确，
+	 * 瞬间转移个永生，只是人工智能，元基编码恰好是这个探索过程的中间bonus产物。
+	 * 
+	 * 2- 文字的描述内容目的非常明确，就是让计算力有浓度。计算力在某种 观测
+	 * 的面上可以用语言的形式来体现。语言是可以提炼的，所以计算力有浓度。比
+	 * 如笛卡尔关系，为了更准确的提炼TVM extension 花肽语指令。可以将源码
+	 * 的这部份提出来，专门设计针对各种语言，输出计算相似的指令片段标注。最终
+	 * 形成自适应的指令片段而不是人为地去定义这个片段。比如目前的8个肽class
+	 * 还是我手写的，之前的3000多花语class也是我手写的。通过仔细分析，不难
+	 * 得到很多仅自适应逻辑计算，就可以分类的属性，比如相同片段，相同的词性，
+	 * 相同的含义，。。。在map归纳下都可以谨谨有条地归纳出来。
+	 * 
+	 * 
+	 * EN 2- Type of observation, to raise and make an ability
+	 * of computation more ratios of concentration. Assumption a
+	 * format of An ability of computation could be a detailed
+	 * human's literacy-languages. The languages have been
+	 * extracting the purity of cognition every times since were
+	 * used, the ability of computation has been mining well in
+	 * production's efficiency-domain, mean a same time. And
+	 * recently one of the effect way of making a promotion of
+	 * exporting out the Cartesian's relationship-logic and
+	 * build an especially test model, to better for continuing
+	 * sub-designs. Thus model could do well in self-adapted and
+	 * initialed as valuable sections of TVM's derive-construction.
+	 * 
+	 * Could start a few sections work below.
+	 *  
+	 * package name-- package S_A.SixActionMap; 
+	 * file name-- WorkVerbaMap 
+	 * function name-- findSubject
+	 * 
+	 * Yaoguang.Luo/罗瑶光
+	 */
+	// to do a swap..  
+	public HashMap<String, String> getCartesianRelationShipFromHumanTalk(
+			String sentence) {
+		return null;
+
+	}
+
+	public HashMap<String, String> getCartesianRelationShipFromHumanTalks(
+			String[] sentences) {
+		return null;
+	}
+
+	public HashMap<String, String> getCartesianPromotionTVMFromRelationShips(
+			HashMap<String, String> relationShips) {
+		return null;
+	}
+	
+	public HashMap<String, String> getUtilsOfCartesianPromotionTVM(
+			HashMap<String, String> cartesianPromotion) {
+		return null;
+	}
+
+	public static void main(String[] argv) {
+
+	}
+
+}
