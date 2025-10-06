26204500c145b54e1f89cb7cebd226a86f8bb50f
luoyaoguang430181198505250014 <yaoguangluo@outlook.com>
2025-10-06 16:28:23 +0800

    1 准备设计关于log的最快观测编码方式规范，方便我以后快速检阅。
    ---------------------------
    个人著作权人, 第一作者: 罗瑶光, 1985年5月25日出生，性别男, 湖南省浏阳市人.
    --浏阳德塔软件开发有限公司-创始人，法人，总经理-- 永久非盈利--
    yaoguangluo@outlook.com, 313699483@qq.com, 2080315360@qq.com,
    （lyg.tin@gmail.com2018年后因G网屏蔽不再使用）
    15116110525-（唯一标识
    2018年2月开始使用此手机卡至今从未拓机，欠费和关机，手机是实名身份证发票机，警惕我家附近类似GSM电子狗频率充斥欺诈）
    430181198505250014, G24402609, EB0581342
    204925063, 389418686, F2406501, 0626136
    当前居住地址: 湖南省 浏阳市 集里街道 神仙坳社区 大塘冲路一段 208号 阳光家园别墅小区 第十栋别墅 第三层
    
    --最近思考家的地址从2009年的北岭美林花苑9号
    改成石霜路阳光花园9号，又石霜路阳光花园10号，又大塘路阳光家园10号，再大塘路一段阳光家园第10栋到现在集里大塘冲路一段208阳光家园别墅小区第10栋，我在想，频繁换名，有问题，于是准确介绍下家地址。
    
    --湖南省浏阳市集里街道-大塘冲路一段208阳光家园别墅小区第10栋--，位于大塘冲路一段208阳光家园小区内的别墅房区入口内左侧，靠近美林花苑入口大门（美林花苑小区内见到阳光家园的最前面金属围栏后便是(10号-面朝建筑左户-罗瑶光家)和（11号-面朝建筑右户）2户联体别墅），美林花苑小区位于大塘冲路一段的十字路口，十字路线的以点轴心斜对面是--浏阳市
    大塘实验(普特融合)小学 建筑--。
    
    --身份证上地址2011年后姑姑罗庆华一家入住。中国身份证件标注地址是户籍地址不是居民地址，罗瑶光先生2018年后一直居住在阳光家园家的第三层。

--- src/test/java/InterfaceTest/chineseParser/DemoEXTest.java
+++ src/test/java/InterfaceTest/chineseParser/DemoEXTest.java
@@ -161,7 +161,7 @@ class DemoEXTest {
 		List<String> setsB = new ArrayList<>();
 		// IMV_SIQ_SS seta = new IMV_SIQ_SS();
 		// loop测试---------------------------------------------------------
-		for (int i = 0; i < ss.length; i++) {
+		for (int i = 0; i < 67; i++) {
 			StringBuilder sb = new StringBuilder(ss[i]);
 			// 执行分词-输出 list格式 的 sets变量
 			TimeCheck t = new TimeCheck();
@@ -178,6 +178,7 @@ class DemoEXTest {
 			Log.logger.info("输入->" + ss[i]);
 			// 输出和统计观测开始------
 			Log.logger.info("输出->");
+			String setsTemp = "";
 			for (int j = 0; j < sets.size(); j++) {
 				if (sets.get(j) != null// 不为null
 					&& !sets.get(j).equals(" ")// 不为空
@@ -187,22 +188,30 @@ class DemoEXTest {
 						score[i] += 1;
 						totalVerbals++;
 						totalSegbals++;
-						System.out.print(sets.get(j) + "=");
+						setsTemp += sets.get(j) + "=";
+						//System.out.print(sets.get(j) + "=");
 					} else {
 						totalVerbals++;
-						System.out.print(sets.get(j) + "x");
+						setsTemp += sets.get(j) + "x";
+						//System.out.print(sets.get(j) + "x");
 					}
 				}
+			}
+			Log.logger.info(setsTemp);
+			if (setsTemp.isEmpty()) {
+				continue;
 			}
 			String size = verbal[i].get("-size-");
 			double sizeD = Double.valueOf(size);
 			score[i] = score[i] / sizeD;
 			Log.logger.info("");
-			Log.logger.info("数据保持单字拆解条件下-分词后过滤新兴潮流代词缩写词人名"
-				+ "等行业专业名词后对比正确率为->" + (score[i] * 100) + "%");
-			Log.logger.info("采样数据可自适应修改。注意保持对比格式的严谨～");
-			// 对比
-			Log.logger.info("理想->" + ss1[i]);
+			//if(score[i] < 1) {
+				Log.logger.info("数据保持单字拆解条件下-分词后过滤新兴潮流代词缩写词人名"
+					+ "等行业专业名词后对比正确率为->" + (score[i] * 100) + "%");
+				Log.logger.info("采样数据可自适应修改。注意保持对比格式的严谨～");
+				// 对比
+				Log.logger.info("理想->" + ss1[i]);	
+			//}
 		}
 		Log.logger.info("共计分词量总数-->" + totalVerbals);
 		Log.logger.info("共计正确分词数-->" + totalSegbals);
